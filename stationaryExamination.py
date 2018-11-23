22ef2a05e4c091ecf1f36904a764f91e7f03c5d6
det2sial <yuanalexiatian@gmail.com>
2018-11-22 23:47:06 -0500

    [ToDo1] Merge the google doc & repo's note

--- stationaryExamination.py
+++ stationaryExamination.py
@@ -3,10 +3,32 @@
 from pandas import Series
 from matplotlib import pyplot
 
-series1 = Series.from_csv('abortion_NYS.csv',header=0)
-series2 = Series.from_csv('abortion_NYC.csv',header=0)
-series1.plot()
-series2.plot()
-pyplot.xlabel("Year")
-pyplot.ylabel("Reported Abortion Number")
+# plot and see trend
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+series = Series.from_csv('abortion_NYC.csv',header=0)
+series.plot()
+pyplot.title('Abortion', fontdict=font)
+pyplot.xlabel('Year',fontdict=font)
+pyplot.ylabel('Reported Rates',fontdict=font)
+pyplot.grid(True)
 pyplot.show()
+
+#log transformation -before parametric statistical test
+
+
+# KPSS (Kwiatkowski-Phillips-Schmidt-Shin) Test
+
+from statsmodels.tsa.stattools import kpss
+#define KPSS
+def kpss_test(timeseries):
+    print ('Results of KPSS Test:')
+    kpsstest = kpss(timeseries, regression='c')
+    kpss_output = pd.Series(kpsstest[0:3], index=['Test Statistic','p-value','Lags Used'])
+    for key,value in kpsstest[3].items():
+    kpss_output['Critical Value (%s)'%key] = value
+print (kpss_output)
