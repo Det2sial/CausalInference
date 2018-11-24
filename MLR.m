d20c6958bed33e5e73d4f3574031de876ad8b870
Grimmer <ytian21@stevens.edu>
2018-11-24 13:17:04 -0500

    [DONE]MLR

---
+++ MLR.m
@@ -0,0 +1,20 @@
+%% Multinomial Logistic Regression
+
+% read csv file and create table
+% header = {'Year','Abortion','DowJones','Incarceration','Crime_Rate'};
+
+data = csvread("E:\code\project\CausalInference-Yuan\regression.csv",1,0);
+year = data(:,1);
+abortion = data(:,2);
+dowjones = data(:,3);
+incarceration = data(:,4);
+crime_rate = data(:,5);
+T = table(year,abortion,dowjones,incarceration,crime_rate);
+
+% multinomial logistic regression
+X = [abortion,dowjones,incarceration];
+Y = categorical(crime_rate);
+% B: coefficicent estimates
+% dev: deviance of the fit
+% stats: model statistics
+[B,dev,stats] = mnrfit(X,Y,'Model','ordinal','link','logit'); 
